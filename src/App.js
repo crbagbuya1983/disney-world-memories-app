4e0f2c981c0e146ff092b42906faf21df2eee3c7
crbagbuya1983 <crbagbuya@gmail.com>
2024-11-26 23:55:32 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MemoryList from './components/MemoryList';
 import MemoryForm from './components/MemoryForm';
 import Favorites from './components/Favorites';
 import Nav from 'react-bootstrap/Nav';
+import backgroundImage from './images/backgroundPicture.png';
 import './App.css';
 
 function App() {
@@ -13,7 +14,19 @@ function App() {
       <Router>
         <div className="App">
           <Header />
-          <main>
+          <main  style={{
+                                        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url(${backgroundImage})`,
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundPosition: 'center center',
+                                        backgroundAttachment: 'fixed',
+                                        backgroundSize: 'cover',
+                                        backgroundColor: 'rgba(255, 255, 255, 0.6)',
+                                        marginTop: '60px',
+                                        marginBottom: '60px',
+                                        borderRadius: '10px',
+                                        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+                                        filter: 'brightness(1)', // Adjust brightness (1 is default, <1 darker, >1 brighter)
+                                    }}>
             <Nav className="nav-container">
               <Nav.Item className="smooth-hover"><Nav.Link as={Link} to="/">Upload Photo</Nav.Link></Nav.Item>
               <Nav.Item className="smooth-hover"><Nav.Link as={Link} to="/view-list">View Gallery</Nav.Link></Nav.Item>
